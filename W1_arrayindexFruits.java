cd8a9bbb615d3d8cf4b7043add72e9c644ea1318
vtu29109 <vtu29109@veltech.edu.in>
2026-09-02 09:40:58 +0530

    Add files via upload

---
+++ W1_arrayindexFruits.java
@@ -0,0 +1,7 @@
+public class Fruits {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Cherry", "Mango"};
+        int index = 2; 
+        System.out.println("Element at index " + index + " is: " + fruits[index]);
+    }
+}
